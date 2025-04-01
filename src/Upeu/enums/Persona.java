c60ae0cced4f77a8694e77b25a9bdac918976455
hrems-dev <hrems077@gmail.com>
2025-04-01 10:33:56 -0500

    aumento

--- src/Upeu/enums/Persona.java
+++ src/Upeu/enums/Persona.java
@@ -1,11 +1,27 @@
 package Upeu.enums;
 
 enum GENERO{Masculino,Femenino};
-enum ESTADO_CIVIL{Casado,Divorciado, Soltero, Viudo}
+
 public class Persona {
+
+    enum ESTADO_CIVIL{Casado,Divorciado, Soltero, Viudo}
     String nombre;
     int edad;
-    String sexo;
-    String estadoCivil;
+    //tring sexo;
+    //String estadoCivil;
 
+    GENERO sexo= GENERO.Masculino;
+    String estado_civil= String.valueOf(ESTADO_CIVIL.Soltero);
+
+    public static void main(String[] args) {
+        Persona p=new Persona();
+        p.nombre="juan";
+        p.edad=30;
+        p.estado_civil=String.valueOf(ESTADO_CIVIL.Casado);
+
+        System.out.println("Nombre: "+p.nombre+"\nEdad: " + p.edad+"\nEstado Civil: "+ p.estado_civil + "\nSexo: "+p.sexo);
+        for (ESTADO_CIVIL e: ESTADO_CIVIL.values()){
+            System.out.println(e);
+        }
+    }
 }
